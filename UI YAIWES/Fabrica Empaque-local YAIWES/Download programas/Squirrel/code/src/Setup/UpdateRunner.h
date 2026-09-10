391dfa25008c2f96521184143ae92b7bbafbfd06
yaiwes-hf-combined-engine <yaiwes-hf-combined-engine@users.noreply.github.com>
2026-09-10 04:57:31 +0000

    build(hf-combined): publish Squirrel bundle and extracted tree

---
+++ UI YAIWES/Fabrica Empaque-local YAIWES/Download programas/Squirrel/code/src/Setup/UpdateRunner.h
@@ -0,0 +1,13 @@
+#pragma once
+class CUpdateRunner
+{
+
+public:
+	static void DisplayErrorMessage(CString& errorMessage, wchar_t* logFile);
+	static HRESULT AreWeInWine();
+	static HRESULT AreWeUACElevated();
+	static HRESULT ShellExecuteFromExplorer(LPWSTR pszFile, LPWSTR pszParameters);
+	static bool DirectoryExists(wchar_t* szPath);
+	static bool DirectoryIsWritable(wchar_t* szPath);
+	static int ExtractUpdaterAndRun(wchar_t* lpCommandLine, bool useFallbackDir);
+};
