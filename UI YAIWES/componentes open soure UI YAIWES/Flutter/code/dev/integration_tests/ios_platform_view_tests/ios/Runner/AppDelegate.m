db61cb172b8ab7e124ec9783347e59d2859c64ce
yaiwes-hf-combined-engine <yaiwes-hf-combined-engine@users.noreply.github.com>
2026-09-10 06:17:11 +0000

    build(hf-combined): publish Flutter bundle and extracted tree

---
+++ UI YAIWES/componentes open soure UI YAIWES/Flutter/code/dev/integration_tests/ios_platform_view_tests/ios/Runner/AppDelegate.m
@@ -0,0 +1,26 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "AppDelegate.h"
+#import "GeneratedPluginRegistrant.h"
+#import "ViewFactory.h"
+#import "TextFieldFactory.h"
+#import "ButtonFactory.h"
+#import "WebViewFactory.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  id<FlutterPluginRegistrar> registrar = [self registrarForPlugin:@"flutter"];
+  [registrar registerViewFactory:[[ViewFactory alloc] init] withId:@"platform_view"];
+  [registrar registerViewFactory:[[TextFieldFactory alloc] init] withId:@"platform_text_field"];
+  [registrar registerViewFactory:[[ButtonFactory alloc] init] withId:@"platform_button"];
+  [registrar registerViewFactory:[[WebViewFactory alloc] init] withId:@"platform_web_view"];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
