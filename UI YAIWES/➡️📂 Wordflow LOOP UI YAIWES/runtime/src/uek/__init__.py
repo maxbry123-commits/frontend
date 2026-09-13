2359540fba9c1f19adae6ad087e6953780de7e6c
Maxbry <maxbry123@gmail.com>
2026-09-12 22:27:27 -0500

    feat(n06): export platform capability classifier

---
+++ UI YAIWES/➡️📂 Wordflow LOOP UI YAIWES/runtime/src/uek/__init__.py
@@ -0,0 +1,27 @@
+from .platform_matrix import (
+    CapabilitySnapshot,
+    Platform,
+    PlatformAssessment,
+    PlatformPolicy,
+    SupportState,
+    assess_all,
+    assess_platform,
+    baseline_linear_policy_lookup,
+    indexed_policy_lookup,
+    policies,
+    policy_fingerprint,
+)
+
+__all__ = [
+    "CapabilitySnapshot",
+    "Platform",
+    "PlatformAssessment",
+    "PlatformPolicy",
+    "SupportState",
+    "assess_all",
+    "assess_platform",
+    "baseline_linear_policy_lookup",
+    "indexed_policy_lookup",
+    "policies",
+    "policy_fingerprint",
+]
