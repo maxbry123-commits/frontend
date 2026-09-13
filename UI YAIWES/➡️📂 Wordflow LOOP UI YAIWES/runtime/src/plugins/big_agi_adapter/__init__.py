23f08cc76d994cebda366efdbb491119b6ecea6e
Maxbry <maxbry123@gmail.com>
2026-09-12 23:36:15 -0500

    feat(n11): add big-AGI adapter package

---
+++ UI YAIWES/➡️📂 Wordflow LOOP UI YAIWES/runtime/src/plugins/big_agi_adapter/__init__.py
@@ -0,0 +1,25 @@
+from .factory import (
+    BIG_AGI_CAPABILITY,
+    BIG_AGI_FACTORY_KEY,
+    BIG_AGI_MOUNT_PATH,
+    BIG_AGI_NAME,
+    BIG_AGI_SOURCE_PATH,
+    BIG_AGI_SOURCE_TREE_SHA,
+    BigAgiAdapterError,
+    BigAgiWorkspace,
+    build_big_agi_factories,
+    create_big_agi_workspace,
+)
+
+__all__ = [
+    "BIG_AGI_CAPABILITY",
+    "BIG_AGI_FACTORY_KEY",
+    "BIG_AGI_MOUNT_PATH",
+    "BIG_AGI_NAME",
+    "BIG_AGI_SOURCE_PATH",
+    "BIG_AGI_SOURCE_TREE_SHA",
+    "BigAgiAdapterError",
+    "BigAgiWorkspace",
+    "build_big_agi_factories",
+    "create_big_agi_workspace",
+]
